b1078db6e2c3e313b4639e9dd4de0b28401f426d
Kevin Gadacz <kevingadacz@gmail.com>
2021-12-08 21:02:06 -0300

    Agrego grilla a consultar producto

--- src/pages/ConsultarProductos.js
+++ src/pages/ConsultarProductos.js
@@ -1,17 +1,113 @@
 import React, { useState, useEffect } from 'react';
+import QuickFilteringGrid from '../components/common/DataGrid';
+import Button from '@mui/material/Button';
+import MenuItem from '@mui/material/MenuItem';
+import Select from '@mui/material/Select';
 
 const ConsultarProductos = () => {
-  // '
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [productos, setProductos] = useState([]);
+  const [versionElegida,setVersionElegida] = useState();
+
+  const contieneProducto = (arr, valor) => {
+    return arr.some((value) => { return value.codigoProducto == valor.codigoProducto });
+  }
+
+  const groupProducts = (data) => {
+    let tempProductos = [];
+    data.map((d) => {
+      if (contieneProducto(tempProductos, d)) {
+        const index = tempProductos.findIndex(value => value.codigoProducto === d.codigoProducto);
+        tempProductos[index].version.push({
+          id: d.id,
+          version: d.version
+        });
+      } else {
+        const prod = {
+          codigoProducto: d.codigoProducto,
+          id: d.id,
+          nombre: d.nombre,
+          version: [
+            {
+              id: d.id,
+              version: d.version
+            }
+          ]
+        }
+        tempProductos.push(prod);
+      }
+    })
+
+    return tempProductos;
+  };
+
+  const onChangeSelectHandler = (version) => {
+      setVersionElegida(version);  
+  }
+
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 110, hide: 'true' },
+    // {field:'codigoProducto',headerName:'Codigo producto', width:110},
+    { field: 'nombre', headerName: 'Nombre', width: 150 },
+    {
+      field: 'version', headerName: 'Version', width: 150,
+      renderCell: (params) => {
+        return (
+          <Select
+            style={{minWidth: 120}}
+            labelId="demo-simple-select-autowidth-label"
+            id="demo-simple-select-autowidth"
+            value={versionElegida}
+          >
+            {params.row.version.map((v) => {
+              return (
+                <MenuItem onClick={() => onChangeSelectHandler(v.version)} value={v.id}>{v.version}</MenuItem>
+              )
+            })}
+
+          </Select >
+        )
+      }
+    },
+    {
+      field: 'acciones', headerName: 'Acciones', width: 300,
+      renderCell: (params) => {
+        const onVerTicketsHandler = (e) => {
+          return alert('hola');
+        };
+        return (
+          <>
+            <Button
+              variant="contained"
+              color="primary"
+              size="small"
+              onClick={onVerTicketsHandler}
+            >
+              Ver tickets
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              size="small"
+              style={{ marginLeft: 16 }}
+              onClick={onVerTicketsHandler}
+            >
+              Crear un nuevo ticket
+            </Button>
+          </>
+        )
+      }
+    }
+  ]
+
   useEffect(() => {
     fetch("https://aninfo-psa-soporte.herokuapp.com/producto")
       .then(res => res.json())
       .then(
         (data) => {
           setIsLoaded(true);
-          setProductos(data);
+          setProductos(groupProducts(data));
         },
         (error) => {
           setIsLoaded(true);
@@ -20,30 +116,8 @@ const ConsultarProductos = () => {
       )
   }, [])
 
-  if (error) {
-    return <div>Error: {error.message}</div>;
-  } else if (!isLoaded) {
-    return <div>Loading...</div>;
-  } else {
-    return (
-      <ul>
-        {productos.map(producto => (
-          <li key={producto.id}>
-            {producto.nombre}
-          </li>
-        ))}
-      </ul>
-    );
-  }
-};
-//   return (
-
-//     <>
-
-//       <h1>Pagina de Consulta de productos</h1>
-//       {getData()}
-//     </>
-//   )
-// };
-
+  return (
+    <QuickFilteringGrid data={productos} columns={columns} />
+  )
+}
 export default ConsultarProductos;
