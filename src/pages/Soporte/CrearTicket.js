a96d76f9c8200fb5731681566da6b3c99eeda233
Kevin Gadacz <kevingadacz@gmail.com>
2021-12-12 15:53:06 -0300

    Align button with form

--- src/pages/Soporte/CrearTicket.js
+++ src/pages/Soporte/CrearTicket.js
@@ -5,15 +5,19 @@ import { useHistory } from "react-router-dom";
 
 const CrearTicket = () => {
   let history = useHistory();
-  
+
   return (
     <>
-      <CrearIncidenciaForm />;
-      <Button variant="contained"
-        color="primary"
-        size="small"
-        style={{ marginLeft: 16 }}
-        onClick={() => history.push('/consultar-productos')}>Volver</Button>
+      <div style={{ padding: 16, margin: 'auto' }}>
+        <CrearIncidenciaForm />
+        <div style={{ marginTop: 16 }}>
+          <Button variant="contained"
+            color="primary"
+            size="small"
+            style={{ marginLeft: 16 }}
+            onClick={() => history.push('/consultar-productos')}>Volver</Button>
+        </div>
+      </div>
     </>
   )
 };
