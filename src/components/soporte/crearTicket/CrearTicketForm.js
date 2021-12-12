861fbef58ed13c5546933383e8ea523c5f3a0292
Kevin Gadacz <kevingadacz@gmail.com>
2021-12-12 18:07:24 -0300

    Agrego mostrar o ocultar asociar tareas en base al tipo de ticket

--- src/components/soporte/crearTicket/CrearTicketForm.js
+++ src/components/soporte/crearTicket/CrearTicketForm.js
@@ -1,8 +1,4 @@
 import react, { useState, useEffect } from 'react';
-import {
-  MuiPickersUtilsProvider,
-} from '@material-ui/pickers';
-import DateFnsUtils from '@date-io/date-fns';
 import { Autocomplete } from '@mui/material';
 import {
   Typography,
@@ -52,6 +48,8 @@ const CrearIncidenciaForm = (props) => {
   const location = useLocation();
   const [clients, setClients] = useState([]);
   const [value, setValue] = React.useState(new Date('2014-08-18T21:11:54'));
+  const [tareas, setTareas] = useState([]);
+  const [esError, setEsError] = useState(false);
 
   const handleChange = (newValue) => {
     setValue(newValue);
@@ -89,8 +87,8 @@ const CrearIncidenciaForm = (props) => {
                       defaultValue="consulta"
                       name="radio-buttons-group"
                     >
-                      <FormControlLabel value="consulta" control={<Radio />} label="Consulta" />
-                      <FormControlLabel value="error" control={<Radio />} label="Error" />
+                      <FormControlLabel value="consulta" control={<Radio />} label="Consulta" onClick={() => setEsError(false)} />
+                      <FormControlLabel value="error" control={<Radio />} label="Error" onClick={() => setEsError(true)} />
                     </RadioGroup>
                   </FormControl>
                 </Grid>
@@ -134,7 +132,7 @@ const CrearIncidenciaForm = (props) => {
                     <MenuItem value="Finalizado"> Finalizado</MenuItem>
                   </Select>
                 </Grid>
-                <Grid item xs={2}/>
+                <Grid item xs={2} />
                 <Grid item xs={5} item style={{ marginTop: 32 }}>
                   <InputLabel required variant="standard" htmlFor="prioridad">
                     Prioridad
@@ -153,6 +151,18 @@ const CrearIncidenciaForm = (props) => {
                     <MenuItem value="S2">S2</MenuItem>
                   </Select>
                 </Grid>
+                <LocalizationProvider item dateAdapter={AdapterDateFns}>
+                  <Grid item style={{ marginTop: 32 }} xs={6}>
+                    <MobileDatePicker
+                      label="Fecha creación"
+                      inputFormat="MM/dd/yyyy"
+                      value={value}
+                      onChange={handleChange}
+                      renderInput={(params) => <TextField {...params} />}
+                    />
+                  </Grid>
+                  <Grid item xs={6} />
+                </LocalizationProvider>
                 <Grid item xs={12} item style={{ marginTop: 16 }}>
                   <Autocomplete
                     disablePortal
@@ -163,18 +173,24 @@ const CrearIncidenciaForm = (props) => {
                     renderInput={(params) => <TextField {...params} label="Cliente" />}
                   />
                 </Grid>
-                <LocalizationProvider item dateAdapter={AdapterDateFns}>
-                    <Grid item style={{ marginTop: 32 }} xs={6}>
-                      <MobileDatePicker
-                        label="Fecha creación"
-                        inputFormat="MM/dd/yyyy"
-                        value={value}
-                        onChange={handleChange}
-                        renderInput={(params) => <TextField {...params} />}
-                      />
-                    </Grid>
-                    <Grid item xs={6} />
-                </LocalizationProvider>
+                { esError &&
+                  <Grid item xs={12} item style={{ marginTop: 16 }}>
+                    <Autocomplete
+                      multiple
+                      id="tags-standard"
+                      options={clients}
+                      getOptionLabel={(option) => option.razonSocial}
+                      renderInput={(params) => (
+                        <TextField
+                          {...params}
+                          variant="standard"
+                          label="Asociar tareas"
+                        />
+                      )}
+                    />
+                  </Grid>
+                }
+
                 <Grid item style={{ marginTop: 32 }}>
                   <Button
                     type="button"
@@ -185,7 +201,7 @@ const CrearIncidenciaForm = (props) => {
                     Reset
                   </Button>
                 </Grid>
-                <Grid item style={{ marginTop: 32, marginLeft:16 }}>
+                <Grid item style={{ marginTop: 32, marginLeft: 16 }}>
                   <Button
                     variant="contained"
                     color="primary"
